4eb0601ce884268edd6d482b2625953b287776e1
Tramaine Gillus <tramaine.gillus@gmail.com>
2019-05-18 22:51:18 -0400

    Refactor.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ElasticsearchHealthCheck = require('./lib/elasticsearch-health-check');
 
 ElasticsearchHealthCheck.getHealthStatuses().then((statuses) => {
-  console.log(statuses)
+  statuses.map(status => console.log(status));
 });
